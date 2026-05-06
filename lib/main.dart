cd6976526056f7986977a1b023aeebc90c951d77
Aidyn55 <alisherza5555@gmail.com>
2026-05-06 13:05:18 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'summary.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +14,47 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
+
+class ArticleModel {
+  Future<Summary> getRandomArticle() async {
+    final uri = Uri.https(
+      'en.wikipedia.com'
+      'api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+    if (response.statusCode != 200) {
+      throw HttpException("Failed to update resourse");
+    }
+    return Summary.fromJson(json.decode(response.body) as Map<String, Object?>);
+  }
+}
+
+class AricleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  Exception? error;
+  bool isloading =false;
+  AricleViewModel(this.model){
+    fetchArticle();
+  }
+  void fetchArticle() async{
+  isloading = true;
+  notifyListeners();
+  try {
+  summary = await model.getRandomArticle();
+  error = null;
+  } on HttpException catch(e){
+  summary = null;
+  error = e;
+  }
+  isloading = false;
+  notifyListeners();
+
+      }
+    }
+
+
