2ee1033b1485fb8c0eb8c370ce2f471abd2cff96
Aidyn55 <alisherza5555@gmail.com>
2026-05-12 14:44:51 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -58,25 +58,47 @@ class AricleViewModel extends ChangeNotifier {
 
 class ArticleWidget extends StatelessWidget {
   final Summary summary;
-  ArticleWidget({super.key, required this.summary})
-  @override
-  Widget build(BuildContext context){
-    return Padding(padding: EdgeInsets.all(8.0),
-    child: Column(
-      spacing: 10,
-      children: [if (summary.hasImage) Image.network(summary.originalImage!.source),
-      Text(
-        summary.titles.normalized,
-        overflow: TextOverflow.ellipsis,
-        style: Theme.of(context).textTheme.displaySmall,
-        ),
-      if (summary.description ! = null)
-      Text(summary.description!
-      overflow: TextOverflow,),
+  ArticleWidget({super.key, required this.summary});
 
-      Text(summary.extract),
-      ],
-    ),
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          Text(summary.extract),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticlePage extends StatelessWidget {
+  final Summary summary;
+  final VoidCallback nextArticle;
+  ArticlePage({super.key, required this.summary, required this.nextArticle});
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(onPressed: nextArticle, child: Text("Next Article")),
+        ],
+      ),
     );
   }
 }
